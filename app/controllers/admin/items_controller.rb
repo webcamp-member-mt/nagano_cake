2e9dacadd1ab48292cea88555609aad01758a2d0
EC2 Default User <ec2-user@ip-172-31-7-40.ap-northeast-1.compute.internal>
2023-01-29 23:21:00 +0000

    [add]admin/itemsの追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,34 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items_index = Item.all
   end
 
   def new
+    @item_new = Item.new
   end
 
   def create
+    @item_new = Item.new(item_params)
+    @item_new.save
+    redirect_to new_admin_item_path
+
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
-  def updateS
+  def update
+    @item = item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to admin_item_path
+    else
+      render :edit
+    end
   end
 end
